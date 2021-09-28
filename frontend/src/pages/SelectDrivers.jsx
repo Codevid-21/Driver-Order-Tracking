042e85a8789b1762606d60a2217cce9d3378eba1
Galip <galipsvt16@gmail.com>
2021-09-28 16:10:29 +0200

    working drivers page 2 component

--- frontend/src/pages/SelectDrivers.jsx
+++ frontend/src/pages/SelectDrivers.jsx
@@ -1,10 +1,8 @@
 import React, { useState, useEffect } from "react";
 import api from "../api/fetchDataFromDB.js";
-import SelectedDrivers from "../components/SelectedDrivers.jsx";
 
 function SelectDrivers() {
   const [drivers, setDrivers] = useState([]);
-  const [checked, setChecked] = useState(false);
 
   const url = `http://localhost:2005/drivers`;
 
@@ -14,32 +12,40 @@ function SelectDrivers() {
     });
   }, [url]);
 
-
-  const handleDrivers = (e) => {
-    e.preventDefault();
-
-    console.log(drivers);
-  };
-
   return (
-    <div>
-      <form onSubmit={handleDrivers}>
-        {drivers.map((value, i) => {
-          value.isWorking = checked;
-          return (
-            <div key={i}>
-              <SelectedDrivers
-                name={value.user.name}
-                _id={value._id}
-                checked={checked}
-                setChecked={setChecked}
-              />
-            </div>
-          );
-        })}
-
-        <button type="submit"> Onayla </button>
-      </form>
+    <div className="allDrivers__container" >
+      <div className="workingDrivers">
+        <div className="workingDrivers__card">
+          <div className="header">
+            <h2>Working Drivers</h2>
+          </div>
+          <div className="workingDrivers__info">
+            {drivers
+              .filter((driver, i) => {
+                return driver.isWorking === true;
+              })
+              .map((value,i) => {
+                return <p key={i} >{value.user.name}</p>;
+              })}
+          </div>
+        </div>
+      </div>
+      <div className="notWorkingDrivers">
+        <div className="notWorkingDrivers__card">
+          <div className="header">
+            <h2>Not Working Drivers</h2>
+          </div>
+          <div className="workingDrivers__info">
+            {drivers
+              .filter((driver, i) => {
+                return driver.isWorking === false;
+              })
+              .map((value,i) => {
+                return <p key={i}  >{value.user.name}</p>;
+              })}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
