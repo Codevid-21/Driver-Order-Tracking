19eee3154489363f9bfd2546097bf81b789efc3d
Yahya Yasir Koca <yykoca.08@gmail.com>
2021-10-01 13:50:32 +0200

    surname deleted

--- backend/models/User.js
+++ backend/models/User.js
@@ -13,55 +13,51 @@ const UserSchema = Schema(
       type: String,
       required: true,
     },
-    surname: {
-        type: String,
-        required: true,
-    },
     email: {
       type: String,
       required: true,
     },
     tel: {
-        type: Number,
-        required: true,
+      type: Number,
+      required: true,
     },
     address: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     city: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
-
-}, { versionKey: false });
+  },
+  { versionKey: false }
+);
 
 const User = mongoose.model("User", UserSchema);
 
 export default {
-    User,
-    readAll: async function () {
-        return await User.find();
-    },
+  User,
+  readAll: async function () {
+    return await User.find();
+  },
   readOne: async function (id) {
     return await User.findById(id);
   },
   findByEmail: async function (email) {
     return await User.find({ email: email });
   },
-    create: async function (name, surname, email, tel, address, city) {
-        console.log("User");
-        const user = new User({
-            name,
-            surname,
-            email,
-            tel,
-            address,
-            city
-
-        });
-        return await user.save();
-    },
+  create: async function (name, surname, email, tel, address, city) {
+    console.log("User");
+    const user = new User({
+      name,
+      surname,
+      email,
+      tel,
+      address,
+      city,
+    });
+    return await user.save();
+  },
   updateByID: async function (id, userObject) {
     console.log("user Object ", userObject);
     // return await User.findById(id);
