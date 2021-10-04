96faf80975c29bedb65e30480fb9b66d6c174bb1
Galip <galipsvt16@gmail.com>
2021-10-04 15:28:00 +0200

    sum page without style

--- frontend/src/pages/Summary.jsx
+++ frontend/src/pages/Summary.jsx
@@ -19,33 +19,29 @@ function Summary() {
     <>
       <div className="summary__container">
         <div className="summary__day">
-          <h5> `Toplam Kac Siparis Götürüldü?? : 5`</h5>
-          <h5> `Toplam Kac Söför calisti?? : 3`</h5>
-          <h5> `Toplam Kac Para toplandi?? : 500`</h5>
+          <h5> Toplam Kac Siparis Götürüldü?? : 5</h5>
+          <h5> Toplam Kac Söför calisti?? : 3</h5>
+          <h5> Toplam Kac Para toplandi?? : 500</h5>
         </div>
 
-        <div className="summary__drivers">
-          <div className="drivers__endDayCard">
-            {drivers.map((driver, i) => {
-              return (
-                <div className="card__items">
-                  <h6>
-                    {driver.user.name} {console.log(driver.deliveries)}{" "}
-                  </h6>
-                  <p>
-                    {`Toplam Teslim Ettigi Siparis :  ${driver.deliveries.length}`}
-                  </p>
-                  Toplam Siparis Tutari :
-                  {driver.deliveries.reduce(
-                    (previousValue, currentValue) =>
-                      previousValue + parseInt(currentValue.total),
-                    0
-                  )}
-                  €
-                </div>
-              );
-            })}
-          </div>
+        <div className="drivers__endDayCard">
+          {drivers.map((driver, i) => {
+            return (
+              <div className="card__items">
+                <h6>
+                  {driver.user.name} {console.log(driver.deliveries)}{" "}
+                </h6>
+                <p>Toplam Teslim Ettigi Siparis : {driver.deliveries.length}</p>
+                Toplam Siparis Tutari :
+                {driver.deliveries.reduce(
+                  (previousValue, currentValue) =>
+                    previousValue + parseInt(currentValue.total),
+                  0
+                )}
+                €
+              </div>
+            );
+          })}
         </div>
       </div>
     </>
