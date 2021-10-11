4772a140f1629ccbfdcf369f1b7db511631d3aaf
Yahya Yasir Koca <yykoca.08@gmail.com>
2021-10-11 20:21:08 +0200

    user model changed

--- backend/models/User.js
+++ backend/models/User.js
@@ -17,6 +17,9 @@ const UserSchema = Schema(
       type: String,
       required: true,
     },
+    password: {
+      type: String
+    },
     tel: {
       type: String, // Girilen bilginin telefon numarasi olup olmadigi kontrol edilecek.
       required: true,
@@ -29,6 +32,12 @@ const UserSchema = Schema(
       type: String,
       required: true,
     },
+    isAdmin: {
+      type: Boolean,
+    },
+    // tip: {
+    //   type: String,
+    // },
   },
   { versionKey: false }
 );
@@ -43,18 +52,34 @@ export default {
   readOne: async function (id) {
     return await User.findById(id);
   },
+  login: async function ({ email, password }) {
+    const user = await User.find({ email: email });
+    if (!user) throw new Error("user_not_found");
+    
+    if (!user[0].isAdmin) throw new Error("user_not_admin");
+    
+    console.log("user bulundu", user)
+    // const isPasswordCorrect = await bcrypt.compare(password.toString() + process.env.PEPPER, user.password);
+    const isPasswordCorrect = (password === user[0].password);
+    if (!isPasswordCorrect) throw new Error("password_incorrect");
+
+    // return { userId: user[0]._id, name: user[0].name, tip: user[0].tip };
+    return { userId: user[0]._id, name: user[0].name, password: user[0].password };
+  },
   findByEmail: async function (email) {
     return await User.find({ email: email });
   },
-  create: async function (name, surname, email, tel, address, city) {
+  create: async function ({name, surname, email, password, tel, address, city, isAdmin}) {
     console.log("User");
     const user = new User({
       name,
       surname,
       email,
+      password,
       tel,
       address,
       city,
+      isAdmin
     });
     return await user.save();
   },
