184daced76eefefeb23ec1c9b166a79eb1a5758a
Galip <galipsvt16@gmail.com>
2021-10-13 14:19:16 +0200

    upload icon to driver image

--- frontend/src/components/newDriver/NewDriverAvatar.jsx
+++ frontend/src/components/newDriver/NewDriverAvatar.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
+import { FiUpload } from "react-icons/fi";
 
 function NewDriverAvatar({ onImageSelect }) {
   const [file, setFile] = useState(null);
 
-  const onImageChange = event => {
+  const onImageChange = (event) => {
     const url = URL.createObjectURL(event.target.files[0]);
     setFile(url);
     onImageSelect(event.target.files[0]);
-  }
+  };
 
   return (
     <div class="container">
@@ -19,12 +20,14 @@ function NewDriverAvatar({ onImageSelect }) {
             accept=".png, .jpg, .jpeg"
             onChange={onImageChange}
           />
-          <label for="imageUpload"></label>
+          <label for="imageUpload">
+            <FiUpload fontSize="20px"  />
+          </label>
         </div>
 
         <div class="avatar-preview">
           <div id="imagePreview">
-            <img src={file ?? "/avatar.png"} alt="" />
+            <img src={file ?? "/avatar.jpeg"} alt="" />
           </div>
         </div>
       </div>
