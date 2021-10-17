0891befcd95fd6fbf579925e3bb569f7d230decd
Galip <galipsvt16@gmail.com>
2021-10-17 17:47:00 +0200

    filter and search func für delivered Orders

--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -1,23 +1,42 @@
 import React, { useEffect, useState } from "react";
 import OrderCard from "../components/OrderCard.jsx";
 import api from "../api/fetchDataFromDB.js";
+import SearchSomething from "../components/SearchSomething.jsx";
 
 function Orders() {
   const [orderInfo, setOrderInfo] = useState([]);
+  const [searchItem, setSearchItem] = useState("");
 
-  const url = `http://localhost:2005/orders`;
-
-  useEffect(() => {
+  const callTheOrder = () => {
+    const url = `http://localhost:2005/orders`;
     api.fetchDataFromDB(url).then((result) => {
       const deliveredOrders = result.filter(
         (value, index) => value.isDelivered === true
       );
-      setOrderInfo(deliveredOrders);
-      console.log("useeffect orders.jsx")
+      setOrderInfo(deliveredOrders.reverse());
     });
-  }, [url]);
+  };
+
+  useEffect(() => {
+    if (searchItem !== "") {
+      const filteredOrders = orderInfo.filter((order) => {
+        return Object.values(order.customerId.user)
+          .join(" ")
+          .toLowerCase()
+          .includes(searchItem.toLowerCase());
+      });
+
+      setOrderInfo(filteredOrders);
+
+      console.log(filteredOrders);
+    } else {
+      callTheOrder();
+    }
+  }, [searchItem]);
+
   return (
     <div>
+      <SearchSomething searchItem={searchItem} setSearchItem={setSearchItem} />
       <OrderCard orderInfo={orderInfo} />
     </div>
   );
